102a36a1cb99afe31d24e4aa15e44d61e670242c
manallouali <manallouali99@gmail.com>
2026-04-24 19:30:55 +0100

    Update Server.js

--- urika_backend/Server.js
+++ urika_backend/Server.js
@@ -32,7 +32,7 @@ initVectorStore()
 const app  = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', methods: ['GET','POST'] }))
+app.use(cors());
 app.use(express.json())
 
 // File upload
